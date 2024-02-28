af683537be1819435557246b16c65a24191eda8c
Daniel Andres Tobon Comba <toboncombadanielandres@gmail.com>
2024-02-28 15:32:52 -0500

    Validaciones , arreglos de objetos y borrado de datos de entradas

--- js/index.js
+++ js/index.js
@@ -2,15 +2,18 @@ import { guardar, obtenerDatos, actualizar } from './firebase.js'
 
 window.addEventListener("DOMContentLoaded", async () => {
 
-    
-
-
 })
 
-
-let arregloLuz = ["Codensa", "Enel"]
+/* let arregloLuz = ["Codensa", "Enel"]
 let arregloAgua = ["Metropolitana", "Codensa"]
-let arregloGas = ["Terpel", "Enel"]
+let arregloGas = ["Terpel", "Enel"] */
+
+let arregloLuz = [{"Codensa":{"link": "https://colombochilena.com/public/uploads/2015/12/logo-codensa.jpg" }} , {"Enel":{"link": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Enel_Group_logo.svg/1024px-Enel_Group_logo.svg.png" }}]
+
+let arregloAgua = [{"Metropolitana":{"link": "https://upload.wikimedia.org/wikipedia/commons/9/9b/Logo_Metropolitane_Italia.svg" }}, {"Codensa":{"link": "https://colombochilena.com/public/uploads/2015/12/logo-codensa.jpg" }}]
+
+let arregloGas = [{"Terpel":{"link": "https://portalcolombia.terpel.com/static/images/terpel_logo_og.png" }}, {"Enel":{"link": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Enel_Group_logo.svg/1024px-Enel_Group_logo.svg.png" }}]
+
 
 
 let cod = ""
@@ -19,9 +22,46 @@ let direccion =  ""
 let logo =  ""
 let valor =  ""
 
+
+//Variables globales 
+let link = ""
+let etiquetaImg = ""
+
+
 const campoServicios = document.getElementById("tipoServicio") 
 const botonPagar = document.getElementById("botonPagar")
 const botonConsultar = document.getElementById("botonConsultar")
+const cerrarConsulta = document.getElementById("cerrarConsulta")
+const cerrarDatosFactura = document.getElementById("cerrarDatosFactura")
+
+
+
+cerrarConsulta.addEventListener ("click" , async () => {
+
+    let contenedorCodigo = document.getElementById("codigoFactura")
+    contenedorCodigo.value = "Ingrese codigo de la factura"
+
+
+})
+
+cerrarDatosFactura.addEventListener("click" , async () => {
+
+
+    let contenedorCodigo = document.getElementById("numeroFactura")
+    let contenedorPeriodo = document.getElementById("periodoFacturado")
+    let contenedorDirrec = document.getElementById("direccionDomicilio")
+    let contenedorLogo = document.getElementById("logoEmpresa")
+    let contenedorPagar = document.getElementById("totalPagar")
+
+
+    contenedorCodigo.textContent = ""   
+    contenedorPeriodo.textContent = "" 
+    contenedorDirrec.textContent = ""
+    contenedorPagar.textContent = ""
+    contenedorLogo.innerHTML = ""
+    
+})
+
 
 
 
@@ -35,29 +75,62 @@ campoServicios.addEventListener("click" , async () => {
 
     if (tipoServicio == "Agua") {
 
-        arregloAgua.forEach(empresa => {
+        for (const dato of arregloAgua) {
+            // Obtener las llaves y valores de cada objeto
+            for (const [llave, valor] of Object.entries(dato)) {
+              console.log("Llave:", llave);
+              console.log("Valor:", valor.link);
+              opciones += ` <option>${llave}</option> `
+              
+            }
+          }
+
+
+        /* arregloAgua.forEach(empresa => {
 
             opciones += ` <option>${empresa}</option> `
 
-        })
+        }) */
 
 
     } else if (tipoServicio == "Luz") {
 
-        arregloLuz.forEach(empresa => {
+
+        for (const dato of arregloLuz) {
+            // Obtener las llaves y valores de cada objeto
+            for (const [llave, valor] of Object.entries(dato)) {
+              console.log("Llave:", llave);
+              console.log("Valor:", valor.link);
+              opciones += ` <option>${llave}</option> `
+              
+            }
+          }
+
+        /* arregloLuz.forEach(empresa => {
 
             opciones += ` <option>${empresa}</option> `
 
-        })
+        }) */
 
 
-    } else {
+    } else if (tipoServicio == "Gas") {
 
-        arregloGas.forEach(empresa => {
+
+        for (const dato of arregloGas) {
+            // Obtener las llaves y valores de cada objeto
+            for (const [llave, valor] of Object.entries(dato)) {
+              console.log("Llave:", llave);
+              console.log("Valor:", valor.link);
+              opciones += ` <option>${llave}</option> `
+              
+            }
+          }
+
+        /* arregloGas.forEach(empresa => {
 
             opciones += ` <option>${empresa}</option> `
 
-        })
+        }) */
 
     }
 
@@ -67,12 +140,63 @@ campoServicios.addEventListener("click" , async () => {
 
 botonConsultar.addEventListener("click" , async (e) =>{
 
+
+    
+
     let tipoServicio = document.getElementById("tipoServicio").value
     let proveedor = document.getElementById("seccionProveedores").value
     let codigoFactura = document.getElementById("codigoFactura").value 
+
+
+
     let existe = false
 
     console.log(tipoServicio , proveedor , codigoFactura)
+
+
+    if (tipoServicio == "Agua"){
+
+        for (const dato of arregloAgua) {
+            // Obtener las llaves y valores de cada objeto
+            for (const [llave, valor] of Object.entries(dato)) {
+              if (llave == proveedor){
+                 link = valor.link
+                 console.log("Llave:", llave);
+                 console.log("Valor:", valor.link);
+              }
+              
+              
+            }
+          }
+    
+      }else if (tipoServicio == "Luz"){
+    
+        for (const dato of arregloLuz) {
+            // Obtener las llaves y valores de cada objeto
+            for (const [llave, valor] of Object.entries(dato)) {
+                if (llave == proveedor){
+                    link = valor.link
+                    console.log("Llave:", llave);
+                    console.log("Valor:", valor.link);
+                 }
+              
+            }
+          }
+    
+      } else if (tipoServicio == "Gas"){
+    
+        for (const dato of arregloGas) {
+            // Obtener las llaves y valores de cada objeto
+            for (const [llave, valor] of Object.entries(dato)) {
+                if (llave == proveedor){
+                    link = valor.link
+                    console.log("Llave:", llave);
+                    console.log("Valor:", valor.link);
+                 }
+            }
+          }
+    
+      }
 
     const datosObtenidos = await obtenerDatos(tipoServicio);
 
@@ -83,7 +207,7 @@ botonConsultar.addEventListener("click" , async (e) =>{
         let contenedorCodigo = document.getElementById("numeroFactura")
         let contenedorPeriodo = document.getElementById("periodoFacturado")
         let contenedorDirrec = document.getElementById("direccionDomicilio")
-        //let contenedorLogo = document.getElementById("logoEmpresa")
+        let contenedorLogo = document.getElementById("logoEmpresa")
         let contenedorPagar = document.getElementById("totalPagar")
 
     if (doc.id == codigoFactura && datosFac["Empresa"] == proveedor) {
@@ -94,7 +218,12 @@ botonConsultar.addEventListener("click" , async (e) =>{
         logo = datosFac["mesFact"]
         valor = datosFac["totalPago"]
 
+        etiquetaImg = `
+        <figure class="image is-128x128">
+            <img class="logoEmpresa" src="${link}" alt="Logo de la empresa">
+        </figure>`
 
+        
 
         console.log("DATOS A FACTURA", cod , periodo , direccion , logo , valor);
         console.log(datosFac);
@@ -105,8 +234,8 @@ botonConsultar.addEventListener("click" , async (e) =>{
         contenedorCodigo.textContent = cod
         contenedorPeriodo.textContent = periodo 
         contenedorDirrec.textContent = direccion 
-        //contenedorLogo.innerHTML = logo 
         contenedorPagar.textContent = valor
+        contenedorLogo.innerHTML = etiquetaImg 
 
 
         
@@ -156,7 +285,7 @@ botonPagar.addEventListener("click", async (e) => {
                             //Aqui solo es necesario colocar lo datos a modificar no todo el objeto de nuevo
                             Pagada: true,
                         }, tipoServicio);
-
+                        location.reload()
                         console.log("Factura pagada");
 
                     } catch (error) {

38b0cafeb0cc3b27382a5a9630f9c93978af5996
Daniel Andres Tobon Comba <toboncombadanielandres@gmail.com>
2024-02-27 21:00:21 -0500

    Obtener datos e imprimirlos

--- js/index.js
+++ js/index.js
@@ -1,59 +1,194 @@
-import {guardar, obtenerDatos , actualizar} from './firebase.js' 
+import { guardar, obtenerDatos, actualizar } from './firebase.js'
 
-window.addEventListener("DOMContentLoaded",  async () => {
+window.addEventListener("DOMContentLoaded", async () => {
+
+    
+
+
 })
 
-const taksform = document.getElementById("task-form")
 
-taksform.addEventListener("submit", async (e) => {
-    e.preventDefault()
+let arregloLuz = ["Codensa", "Enel"]
+let arregloAgua = ["Metropolitana", "Codensa"]
+let arregloGas = ["Terpel", "Enel"]
+
+
+let cod = ""
+let periodo =  ""
+let direccion =  ""
+let logo =  ""
+let valor =  ""
+
+const campoServicios = document.getElementById("tipoServicio") 
+const botonPagar = document.getElementById("botonPagar")
+const botonConsultar = document.getElementById("botonConsultar")
+
+
+
+campoServicios.addEventListener("click" , async () => {
+
+    let tipoServicio = document.getElementById("tipoServicio").value
+
+    let opciones = ""
+
+    let contenedor = document.getElementById("seccionProveedores")
+
+    if (tipoServicio == "Agua") {
+
+        arregloAgua.forEach(empresa => {
+
+            opciones += ` <option>${empresa}</option> `
+
+        })
+
+
+    } else if (tipoServicio == "Luz") {
+
+        arregloLuz.forEach(empresa => {
+
+            opciones += ` <option>${empresa}</option> `
+
+        })
+
+
+    } else {
+
+        arregloGas.forEach(empresa => {
+
+            opciones += ` <option>${empresa}</option> `
+
+        })
+
+    }
+
+    contenedor.innerHTML = opciones
+})
+
+
+botonConsultar.addEventListener("click" , async (e) =>{
+
+    let tipoServicio = document.getElementById("tipoServicio").value
+    let proveedor = document.getElementById("seccionProveedores").value
+    let codigoFactura = document.getElementById("codigoFactura").value 
     let existe = false
-    const codigoFactura = taksform['codigo-factura'].value
-    const proveedor = taksform['proveedor'].value
-    const servicio = taksform['servicio'].value
+
+    console.log(tipoServicio , proveedor , codigoFactura)
+
+    const datosObtenidos = await obtenerDatos(tipoServicio);
+
+    datosObtenidos.forEach(async doc => {
+
+        const datosFac = doc.data();
+
+        let contenedorCodigo = document.getElementById("numeroFactura")
+        let contenedorPeriodo = document.getElementById("periodoFacturado")
+        let contenedorDirrec = document.getElementById("direccionDomicilio")
+        //let contenedorLogo = document.getElementById("logoEmpresa")
+        let contenedorPagar = document.getElementById("totalPagar")
+
+    if (doc.id == codigoFactura && datosFac["Empresa"] == proveedor) {
+
+        cod = doc.id
+        periodo = datosFac["mesFact"]
+        direccion = datosFac["direccion"]
+        logo = datosFac["mesFact"]
+        valor = datosFac["totalPago"]
+
+
+
+        console.log("DATOS A FACTURA", cod , periodo , direccion , logo , valor);
+        console.log(datosFac);
+        console.log("Factura encontrada");
+
+       
+
+        contenedorCodigo.textContent = cod
+        contenedorPeriodo.textContent = periodo 
+        contenedorDirrec.textContent = direccion 
+        //contenedorLogo.innerHTML = logo 
+        contenedorPagar.textContent = valor
+
+
+        
+        existe = true
+
+        }
+
+    })
+
+    e.preventDefault()
+
+})
+
+botonPagar.addEventListener("click", async (e) => {
+
+    let existe = false
+
+    
+    e.preventDefault()
+
+
+    if (codigoFactura == "" || proveedor == "" || tipoServicio == "") {
+        alert("Datos invalidos")
+        return
+
+    }else{
+
+        const datosObtenidos = await obtenerDatos(tipoServicio);
+
+        datosObtenidos.forEach(async doc => {
+
+            const datosFac = doc.data();
+
+            if (doc.id == codigoFactura && datosFac["Empresa"] == proveedor) {
+                console.log("ID:", doc.id);
+                console.log(datosFac);
+                console.log("Factura encontrada");
+                existe = true
+
+                if (!datosFac.Pagada) {
+                    try {
+                        await actualizar(doc.id, {
+                            //Aqui solo es necesario colocar lo datos a modificar no todo el objeto de nuevo
+                            Pagada: true,
+                        }, tipoServicio);
+
+                        console.log("Factura pagada");
+
+                    } catch (error) {
+                        console.error("Error al actualizar la factura:", error);
+
+                    }
+                } else {
+                    console.log("La factura ya está pagada");
+
+                }
+            }
+        })
+
+        if (!existe)
+            alert("La factura no existe")
+
+    }
+
+
+    
+
+
+
+
+
+
+    /* 
 
     //datos(codigoFactura , proveedor , servicio) 
 
-    if (codigoFactura == "" || proveedor == "" || servicio == ""){
-        alert("Datos invalidos")
-        return
-    }
-
-    const datosObtenidos = await obtenerDatos(servicio);
-
-    datosObtenidos.forEach(async doc => {
-        const datosFac = doc.data();
-
-        if (doc.id == codigoFactura && datosFac["Empresa"] == proveedor) {
-            console.log("ID:", doc.id);
-            console.log(datosFac);
-            console.log("Factura encontrada");
-            taksform.reset()
-            existe = true
-
-            if (!datosFac.Pagada) {
-                try {
-                    await actualizar(doc.id, {
-                        Empresa: proveedor,
-                        Pagada: true,
-                    } , servicio );
-
-                    console.log("Factura pagada");
-                    
-                } catch (error) {
-                    console.error("Error al actualizar la factura:", error);
-                    
-                }
-            } else {
-                console.log("La factura ya está pagada");
-                
-            }
-        }
-    })
-
-    if (!existe) 
-        alert("La factura no existe")
+    
 
     
+
+     */
+
+
 
 }) 

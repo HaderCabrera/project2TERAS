9e6275f769c63a03deca96bd6d6ed359eb05739e
diegodfp <diegodfp.fly@gmail.com>
2024-02-27 21:57:05 -0500

    validacion agregada

--- db/index.js
+++
@@ -1,9 +0,0 @@
-import { getTask } from "./firebase.js";
-
-window.addEventListener('DOMContentLoaded', async() =>{
-    console.log("estoy aqui")
-    const querySnapshot = await getTask()
-    querySnapshot.forEach(doc =>{
-        console.log(doc)
-    })
-})
